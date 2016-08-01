8c7b8d5bb805274662159e94b4a16e40c50f254c
Martin Kagume <martiniriga@gmail.com>
2016-08-02 01:49:13 +0300

    working

--- EasyPay/HomePage.xaml.cs
+++ EasyPay/HomePage.xaml.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Data.Json;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
 using Windows.UI;
 using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +33,10 @@ namespace EasyPay
     public sealed partial class HomePage : Page
     {
         Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+        List<PhoneQRs> QrList = new List<PhoneQRs>();
+        public string Code { get; set; }
+
         // temporary variable for holding the latest textBox value before the textChange event is trigerred
         string txtTemp = "";
         // this boolean is to be used by the textChanged event to decide to accept changes or not
@@ -43,37 +55,84 @@ namespace EasyPay
             txtTemp = txtPhoneNo.Text;
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
+            //clear verification code
+            txtVerify.Text = "";
             if (localSettings.Values["PhoneNo"] != null)
             {
-                txtPhoneNo.Text = localSettings.Values["PhoneNo"].ToString();
-                btnGenerateCode.IsEnabled = true;
+                txtPhoneNo.Text = localSettings.Values["PhoneNo"].ToString(); 
+                //retrieve image
+                string fileName = "MyQr.png";
+                StorageFolder myfolder = ApplicationData.Current.LocalFolder;
+                BitmapImage bitmapImage = new BitmapImage(); 
+                try
+                {
+                    StorageFile file = await myfolder.GetFileAsync(fileName);
+                    var image = await Windows.Storage.FileIO.ReadBufferAsync(file);
+                    Uri uri = new Uri(file.Path);
+                    BitmapImage img = new BitmapImage(new Uri(file.Path));
+                    QRCode.Source = img;
+                }
+                catch (FileNotFoundException)
+                {
+                    UIHelper.ShowAlert("Missing QR Image");
+                }
+              
+            }
+            else
+            {
+                btnVerify.IsEnabled = true;
             }
            
 
         }
 
 
-        private void btnGenerateCode_Click(object sender, RoutedEventArgs e)
+        private async void btnGenerateCode_Click(object sender, RoutedEventArgs e)
         {
-            //store phoneNo
-            localSettings.Values["PhoneNo"] = txtPhoneNo.Text;
-
-            IBarcodeWriter writer = new BarcodeWriter
+            if (!Code.Equals(txtVerify.Text,StringComparison.CurrentCultureIgnoreCase))
             {
-                Format = BarcodeFormat.QR_CODE,//Mentioning type of bar code generation   
-                Options = new ZXing.Common.EncodingOptions
+                await UIHelper.ShowAlert("Invalid code");
+            }
+            else
+            {
+                //store phoneNo
+               // localSettings.Values["PhoneNo"] = txtPhoneNo.Text;
+
+                IBarcodeWriter writer = new BarcodeWriter
                 {
-                    Height = 300,
-                    Width = 300
-                },
-                Renderer = new ZXing.Rendering.PixelDataRenderer() { Foreground = Colors.Black,Background = Colors.LightBlue }//Adding color QR code   
-            };
-            var result = writer.Write(txtPhoneNo.Text);
-            var wb = result.ToBitmap() as WriteableBitmap;
-            //Displaying QRCode Image   
-            QRCode.Source = wb;   
+                    Format = BarcodeFormat.QR_CODE,//Mentioning type of bar code generation   
+                    Options = new ZXing.Common.EncodingOptions
+                    {
+                        Height = 300,
+                        Width = 300
+                    },
+                    Renderer = new ZXing.Rendering.PixelDataRenderer() { Foreground = Colors.Black, Background = Colors.Silver }//Adding color QR code   
+                };
+                var result = writer.Write(txtPhoneNo.Text);
+                var wb = result.ToBitmap() as WriteableBitmap;
+
+                //Displaying QRCode Image   
+                QRCode.Source = wb;
+                await UIHelper.ShowAlert("QR Generated Successfully");
+                //empty verify text field
+                txtVerify.Text = "";
+                StorageFolder folder = ApplicationData.Current.LocalFolder;
+                if (folder != null)
+                {
+                    StorageFile file = await folder.CreateFileAsync("MyQr" + ".png", CreationCollisionOption.ReplaceExisting);
+                    using (var storageStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                    {
+                        var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, storageStream);
+                        var pixelStream = wb.PixelBuffer.AsStream();
+                        var pixels = new byte[pixelStream.Length];
+                        await pixelStream.ReadAsync(pixels, 0, pixels.Length);
+                        encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Ignore, (uint)wb.PixelWidth, (uint)wb.PixelHeight, 48, 48, pixels);
+                        await encoder.FlushAsync();
+                    }
+                }
+            }
         }
 
         private void txtPhoneNo_GotFocus(object sender, RoutedEventArgs e)
@@ -93,9 +152,19 @@ namespace EasyPay
 
             //enable btngenerate
             if (txtPhoneNo.Text.Length == 12)
-                btnGenerateCode.IsEnabled = true;
+            {
+                btnVerify.IsEnabled = true;
+               
+            }
             else
-                btnGenerateCode.IsEnabled = false;
+            {
+                btnVerify.IsEnabled = true;
+            }
+            if (!txtPhoneNo.Text.StartsWith("254"))
+            {
+                txtPhoneNo.Text = "254";
+            }  
+
             System.Diagnostics.Debug.WriteLine("KeyPress " + Convert.ToChar(args.KeyCode) + "keyCode = " + args.KeyCode.ToString());
             args.Handled = true;
 
@@ -113,7 +182,8 @@ namespace EasyPay
 
         private void txtPhoneNo_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            if(btnVerify.Visibility == Visibility.Collapsed)
+                btnVerify.Visibility = Visibility.Visible;
             if (txtPhoneNo.Text.Length < 13)
             {
                 if (acceptChange)
@@ -137,10 +207,78 @@ namespace EasyPay
             }
         }
 
-       
 
+        private async void btnVerify_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtPhoneNo.Text.Length != 12 || !IsDigitsOnly(txtPhoneNo.Text))
+            {
+                await UIHelper.ShowAlert("Invalid Phone number");
+            }
+            else
+            {
+                await PostAsync();
+            }
+                
+        }
 
+        bool IsDigitsOnly(string str)
+        {
+            foreach (char c in str)
+            {
+                if (c < '0' || c > '9')
+                    return false;
+            }
 
+            return true;
+        }
 
+        private async Task PostAsync()
+        {
+            var url = "http://139.59.186.10/mpesa/public/verify-phone?phone_number=" + txtPhoneNo.Text;       
+
+            // client...
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            UIHelper.ToggleProgressBar(true, "Verifying code from server..");
+            var response = await client.GetAsync(url);
+
+            // load it up...
+            var outputJson = await response.Content.ReadAsStringAsync();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                JObject output = JObject.Parse(outputJson);
+                String status = (string)output["status"];
+                if (status != "error")
+                {                    
+                    Code = (string)output["verification_code"]; 
+                    await UIHelper.ShowAlert("Enter Received Code");
+                    btnVerify.Visibility = Visibility.Collapsed;
+                    txtVerify.Focus(FocusState.Keyboard);
+                }
+                else
+                {
+                    string error = (string)output["error"]["code"];
+                    await UIHelper.ShowAlert(error);
+                }
+               
+            }
+            UIHelper.ToggleProgressBar(false);
+
+        }
+
+        private void txtVerify_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(btnGenerateCode.IsEnabled == false)
+             btnGenerateCode.IsEnabled = true;
+        }
+
+        
+
+    }
+
+    class PhoneQRs
+    {
+        public string UserType { get; set; }
+        public string PhoneNo { get; set; }
     }
 }
---
+++ EasyPay/Objects/UIHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace EasyPay
+{
+    public static class UIHelper
+    {
+        public static async void ToggleProgressBar(bool toggle, string message = "")
+        {
+            var statusBar = Windows.UI.ViewManagement.StatusBar.GetForCurrentView();
+
+            if (toggle)
+            {
+                statusBar.ProgressIndicator.Text = message;
+                await statusBar.ProgressIndicator.ShowAsync();
+            }
+            else
+            {
+                await statusBar.ProgressIndicator.HideAsync();
+            }
+        }
+
+        public static async Task ShowAlert(string message)
+        {
+            MessageDialog dialog = new MessageDialog(message);
+            await dialog.ShowAsync();
+        }
+    }
+}
